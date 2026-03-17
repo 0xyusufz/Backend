42943db9d127311c196be841984eb24bc37380f8
0xyusufz <gurucoc471@gmail.com>
2026-03-17 10:06:45 +0530

    database connected

--- day6/server.js
+++ day6/server.js
@@ -1,4 +1,15 @@
 const app = require("./src/app")
+const mongoose = require("mongoose")
+
+async function connectDB(){
+    try{
+        await mongoose.connect("mongodb+srv://yusuf:Yusuffatah%4009@cluster0.zypo3og.mongodb.net/day-6")
+        console.log("connected to database")
+    }catch(e){
+        console.log("failed to connect with database")
+    }
+}
+connectDB()
 port =3000
 app.listen(port,()=>{
     console.log(`this server is running at port ${port}`)
