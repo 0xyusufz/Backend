e1c3f4c77c11220033035b3ecc35da76d964c213
0xyusufz <gurucoc471@gmail.com>
2026-03-17 09:42:27 +0530

    database

---
+++ day6/server.js
@@ -0,0 +1,6 @@
+const app = require("./src/app")
+port =3000
+app.listen(port,()=>{
+    console.log(`this server is running at port ${port}`)
+})
+
---
+++ day6/src/app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()   //instance of a express
+app.use(express.json())  //middleware
+let notes =[]
+// post method
+app.post("/notes",(req,res)=>{
+    notes.push(req.body)
+    res.status(201).json({
+        message:"notes created successfuly"
+    })
+})
+// get method
+app.get("/notes",(req,res)=>{
+    res.status(200).json({
+        notes:notes
+    })
+})
+// delete method
+app.delete("/notes/:id",(req,res)=>{
+    delete notes[req.params.id]
+    res.status(204)
+})
+// patch method
+app.patch("/notes/:id",(req,res)=>{
+    notes[req.params.id].desc = req.body.desc
+    res.status(200).json({
+        message:"description updated successfully"
+    })
+})
+module.exports = app
